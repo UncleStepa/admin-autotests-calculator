3cdfe3c89371ead7181c7d3f09b58b73f9abed7c
UncleStepa <stankevichii@gmail.com>
2020-05-18 16:04:14 +0400

    Добавил тест с Базой данных

--- src/main/java/ru/neoflex/dao/MySqlConnector.java
+++ src/main/java/ru/neoflex/dao/MySqlConnector.java
@@ -4,34 +4,22 @@ import java.sql.*;
 
 public class MySqlConnector {
 
-    private static Connection getConnection(){
-      Connection con = null;
+    public static Connection getConnection(){
+      Connection connection = null;
       try {
-        con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/testimony?useUnicode=true&serverTimezone=UTC", "root", "148192");
-        System.out.println("Got our connection");
+        connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/testimony?useUnicode=true&serverTimezone=UTC", "root", "148192");
+        System.out.println("Успешное поключение к базе данных");
       } catch (SQLException e) {
         e.printStackTrace();
+        System.out.println("Ошибка подключения " + e);
       }
-      return con;
+      return connection;
     }
 
 
-  public static void selectAllFrommBilling() throws SQLException {
-    Connection connection = getConnection();
-    Statement st = connection.createStatement();
-    String query = "SELECT * FROM billing_period";
-    ResultSet rs = st.executeQuery(query);
-    while (rs.next()) {
-      int id = rs.getInt("id");
-      String date = rs.getString("currentmonth");
-      int coldWater = rs.getInt("coldWater");
-      int hotWater = rs.getInt("hotWater");
-      int gas = rs.getInt("gas");
-      int electricity = rs.getInt("electricity");
-      System.out.format("%s, %s, %s, %s, %s, %s\n", id, date, coldWater, hotWater, gas, electricity);
-      System.out.println(date);
-    }
-    connection.close();
+  public static ResultSet selectAllFrommBilling(String currentmonth) throws SQLException {
+    Statement st = getConnection().createStatement();
+    ResultSet resultSet = st.executeQuery("SELECT * FROM billing_period WHERE currentmonth = " + currentmonth);
+    return resultSet;
   }
-
 }
--- src/test/java/ru/vtb/neoflex/autotests/SaveTestimonyTest.java
+++ src/test/java/ru/vtb/neoflex/autotests/SaveTestimonyTest.java
@@ -8,13 +8,14 @@ import ru.neoflex.model.CurrentTestimony;
 import ru.neoflex.model.RequestSaveTestimony;
 import ru.neoflex.model.ResponseSaveTestimony;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class SaveTestimonyTest {
   static String saveTestimonyURI = "http://localhost:8080/services/testimony/save";
 
   @Test
-  public void checkCodeSuccessTest() throws SQLException {
+  public void checkCodeSuccessTest() {
     RequestSaveTestimony requestSaveTestimony = new RequestSaveTestimony();
     CurrentTestimony currentTestimony = new CurrentTestimony();
 
@@ -26,14 +27,13 @@ public class SaveTestimonyTest {
     requestSaveTestimony.setCurrentTestimony(currentTestimony);
 
     int actualStatusCode = RequestTestController.getRequestCode(saveTestimonyURI, requestSaveTestimony);
-    System.out.println("statusCode : " + actualStatusCode);
     Assertions.assertEquals(200, actualStatusCode);
-   MySqlConnector.selectAllFrommBilling();
+
   }
 
 
   @Test
-  public void checkFaultCodeSuccessTest()  {
+  public void checkFaultCodeSuccessTest() throws SQLException {
     RequestSaveTestimony requestSaveTestimony = new RequestSaveTestimony();
     CurrentTestimony currentTestimony = new CurrentTestimony();
 
@@ -47,10 +47,21 @@ public class SaveTestimonyTest {
     ResponseSaveTestimony responseSaveTestimony = RequestTestController.getResponseBodySave(saveTestimonyURI, requestSaveTestimony);
     String resultCode = responseSaveTestimony.getFaultcode().getResultCode();
     String resultText = responseSaveTestimony.getFaultcode().getResultText();
-    System.out.println(resultCode);
-    System.out.println(resultText);
     Assertions.assertEquals("0", resultCode);
     Assertions.assertEquals("success", resultText);
 
+    ResultSet expectedRsult = MySqlConnector.selectAllFrommBilling(requestSaveTestimony.getDate());
+    while (expectedRsult.next()) {
+      String date = expectedRsult.getString("currentmonth");
+      double coldWater = expectedRsult.getInt("coldWater");
+      double hotWater = expectedRsult.getInt("hotWater");
+      double gas = expectedRsult.getInt("gas");
+      double electricity = expectedRsult.getInt("electricity");
+      Assertions.assertEquals(date, requestSaveTestimony.getDate());
+      Assertions.assertEquals(coldWater, requestSaveTestimony.getCurrentTestimony().getColdWater());
+      Assertions.assertEquals(hotWater, requestSaveTestimony.getCurrentTestimony().getHotWater());
+      Assertions.assertEquals(gas, requestSaveTestimony.getCurrentTestimony().getGas());
+      Assertions.assertEquals(electricity, requestSaveTestimony.getCurrentTestimony().getElectricity());
+    }
   }
 }
